1b4de28286e462120435697010806cd2f38a56a0
Gabriele Lopez <gabriele.ope@gmail.com>
2023-02-15 19:39:19 +0100

    Question adder

---
+++ Backend/QuestionAdder/Program.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using QuizApi.Classes;
+
+namespace QuestionAdder;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var client = new MongoClient("mongodb://localhost:27017");
+        var database = client.GetDatabase("eduCiv");
+        var questionsDatabase = database.GetCollection<Question>("questions");
+        var list = new List<Answer>()
+        {
+            new Answer("Giglo", false),
+            new Answer("Nicolo", true)
+        };
+        await questionsDatabase.InsertOneAsync(new Question("Barille is barille?", list));
+    }
+}
