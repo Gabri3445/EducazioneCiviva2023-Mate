550fab213ad56ba61b399249f43e323bd221bd77
Gabriele Lopez <gabriele.ope@gmail.com>
2023-02-16 12:53:55 +0100

    Giglo

---
+++ Backend/QuizApi/Controllers/Requests/Requests.cs
@@ -0,0 +1,8 @@
+﻿using SharedClasses;
+
+namespace QuizApi.Controllers.Requests;
+
+public class CreateUserRequest 
+{
+    public string Username { get; set; }
+}
---
+++ Backend/QuizApi/Controllers/Responses/Responses.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizApi.Controllers.Responses;
+
+public class CreateUserResponse
+{
+    public CreateUserResponse(string id)
+    {
+        Id = id;
+    }
+
+    public string Id { get; set; }
+}
