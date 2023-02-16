e19560c12fc43c9b73564ee2d635cb253d7fe32b
Gabriele Lopez <gabriele.ope@gmail.com>
2023-02-16 10:14:07 +0100

    Question adder

--- Backend/QuestionAdder/Program.cs
+++ Backend/QuestionAdder/Program.cs
@@ -10,11 +10,29 @@ public static class Program
         var client = new MongoClient("mongodb://localhost:27017");
         var database = client.GetDatabase("eduCiv");
         var questionsDatabase = database.GetCollection<Question>("questions");
-        var list = new List<Answer>()
+        var exit = false;
+        var questions = new List<Question>();
+        do
         {
-            new Answer("Giglo", false),
-            new Answer("Nicolo", true)
-        };
-        await questionsDatabase.InsertOneAsync(new Question("Barille is barille?", list));
+            Console.WriteLine("Domanda");
+            var question = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Numero di risposte");
+            var num = int.Parse(Console.ReadLine() ?? string.Empty);
+            var answers = new List<Answer>();
+            for (var i = 0; i < num; i++)
+            {
+                Console.WriteLine("Risposta");
+                var answer = Console.ReadLine() ?? string.Empty;
+                Console.WriteLine("Vera? 0 vero, 1 falso");
+                var isTrue = int.Parse(Console.ReadLine() ?? string.Empty) == 0;
+                answers.Add(new Answer(answer, isTrue));
+            }
+
+            questions.Add(new Question(question, answers));
+            Console.WriteLine("0 to continue");
+            exit = int.Parse(Console.ReadLine() ?? string.Empty) == 0;
+        } while (exit);
+
+        await questionsDatabase.InsertManyAsync(questions);
     }
 }
