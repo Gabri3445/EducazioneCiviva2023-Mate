5c1460a17684e3a217348b4cc8c25c22bd54af85
Gabriele Lopez <gabriele.ope@gmail.com>
2023-03-23 08:12:07 +0100

    Added nuget package for windows service hosting

--- Backend/QuizApi/Program.cs
+++ Backend/QuizApi/Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Host.UseWindowsService(options =>
+{
+    options.ServiceName = "QuizServer";
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
